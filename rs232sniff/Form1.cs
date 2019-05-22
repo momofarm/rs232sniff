f836f9e76c2abf431bf42da1f02f375e68266304
tim <timchenwis@gmail.com>
2019-05-22 09:45:42 +0800

    save message

--- rs232sniff/Form1.cs
+++ rs232sniff/Form1.cs
@@ -88,6 +88,46 @@ namespace rs232sniff
             t.Join();
             
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //  let's save current text in textbox
+            string text = richTextBox1.Text;
+
+            opensaveFileDialog(text);
+            
+        }
+
+        private void opensaveFileDialog(string text)
+        {
+            // Displays a SaveFileDialog so the user can save the Image  
+            // assigned to Button2.  
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Text File|*.txt|";
+            saveFileDialog1.Title = "Save an Text File";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.  
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.  
+                System.IO.FileStream fs =
+                   (System.IO.FileStream)saveFileDialog1.OpenFile();
+                // Saves the Image in the appropriate ImageFormat based upon the  
+                // File type selected in the dialog box.  
+                // NOTE that the FilterIndex property is one-based.  
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                    default:
+                        byte[] info = new UTF8Encoding(true).GetBytes(text);
+                        fs.Write(info, 0, info.Length);
+                        break;
+                }
+
+                fs.Close();
+            }
+        }
     }
 
     public class Worker
